29a8d5c60542f030a2b43fb0b5195e1d92cbb4e3
Viktor <Vikman88@gmail.com>
2020-06-10 23:29:40 +0400

    step three

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
 
-import actual from '..';
+import {dialogWithUser} from '..';
+dialogWithUser();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-
-export const actual = readlineSync.question('May I have your name?');
-console.log('Hello, ' + actual + '!');
+export const dialogWithUser = () => {
+	console.log('Welcome to the Brain Games!');
+	const username = readlineSync.question('May I have your name?');
+	console.log('Hello, ' + username + '!');
+	return;
+};
