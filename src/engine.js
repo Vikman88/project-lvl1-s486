c26acb8615f728298b2519f4409e06eb3da65b40
Viktor <Vikman88@gmail.com>
2020-06-22 19:11:01 +0400

    fix code

--- src/engine.js
+++ src/engine.js
@@ -3,28 +3,29 @@ import {getGeneratedNumber} from './numberGenerator.js';
 import {getGeneratedSymbol} from './symbolGenerator.js';
 
 export const constructor = (game) => () => {
+  let question;
+  let answer;
+  const result = (question, answer) => [question, answer];
   switch (game) {
 
     case "even": {
-      const questionEven = getGeneratedNumber();
-      const answerEven = isEvenNumber(questionEven) ? 'yes' : 'no';
-      const resultEven = [_.toString(questionEven), answerEven];
-      return resultEven;
+      question = getGeneratedNumber();
+      answer = isEvenNumber(question) ? 'yes' : 'no';
+      return result(_.toString(question), answer);
     }
 
     case "calc": {
-      const questionCalc = `${getGeneratedNumber()} ${getGeneratedSymbol()} ${getGeneratedNumber()}`;
-      const resultCalc = [questionCalc, _.toString(getExpressionResultFromString(questionCalc))];
-      return resultCalc;
+      question = `${getGeneratedNumber()} ${getGeneratedSymbol()} ${getGeneratedNumber()}`;
+      return result(question, _.toString(getExpressionResultFromString(question)));
     }
 
     case "gcd": {
       const num1 = getGeneratedNumber();
       const num2 = getGeneratedNumber();
-      const questionGcd = `${num1} ${num2}`;
+      question = `${num1} ${num2}`;
       const arr1 = getArrayDividers(num1);
       const arr2 = getArrayDividers(num2);
-      const answerGcd = arr1.reduce((acc, v) => {
+      answer = arr1.reduce((acc, v) => {
         if (arr2.indexOf(v) > -1) {
           const deleteEl = arr2.indexOf(v);
           arr2.splice(deleteEl, 1);
@@ -32,36 +33,37 @@ export const constructor = (game) => () => {
         }
         return acc;
       }, []).reduce((acc, v) => acc *= v, 1);
-      const resultGcd = [questionGcd, _.toString(answerGcd)];
-      return resultGcd;
+      return result(question, _.toString(answer));
     }
 
     case "progression": {
+      const countNumbers = 10;
       const randomNum1 = _.random(1, 10);
       const randomNum2 = _.random(50);
       const hideNum = _.random(9);
-      const questionProgression = _.times(10, (v) => v * randomNum1 + randomNum2);
-      const answerProgression = questionProgression[hideNum];
-      const resultProgression = [convertToString(hideNum, questionProgression), _.toString(answerProgression)];
-      return resultProgression;
+      question = _.times(countNumbers, (v) => v * randomNum1 + randomNum2);
+      answer = question[hideNum];
+      return result(convertToString(hideNum, question), _.toString(answer));
     }
 
     case "prime": {
-      const questionPrime = getGeneratedNumber();
-      const answerPrime = isPrimeNumber(questionPrime) ? 'yes' : 'no';
-      const resultPrime = [_.toString(questionPrime), answerPrime];
-      return resultPrime;
+      question = getGeneratedNumber();
+      answer = isPrimeNumber(question) ? 'yes' : 'no';
+      return result(_.toString(question), answer);
     }
   }
 };
 
 const isEvenNumber = (num) => num % 2 === 0;
 
-const isPrimeNumber = (num, divider = 2) => {
-  if (num === 1) return false;
-  if (num === divider) return true;
-  if (num % divider === 0) return false;
-  return isPrimeNumber(num, divider += 1);
+const isPrimeNumber = (num) => {
+  const iter = (num, divider) => {
+    if (num === 1) return false;
+    if (num === divider) return true;
+    if (num % divider === 0) return false;
+    return iter(num, divider += 1);
+  }
+  return iter(num, 2);
 };
 
 const convertToString = (num, arr) => arr.map((v, i) => (i === num ? '..' : v)).join(' ');
