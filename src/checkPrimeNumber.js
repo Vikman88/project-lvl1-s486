f40872efd4b8233da892bb49b78c96e7cea5d790
Viktor <Vikman88@gmail.com>
2020-06-27 18:19:00 +0400

    brain games 1.0.2

--- src/checkPrimeNumber.js
+++ src/checkPrimeNumber.js
@@ -1,6 +1,6 @@
 export const isPrimeNumber = (num) => {
+  if (num === 1) return false;
   const iter = (number, divider) => {
-    if (number === 1) return false;
     if (number === divider) return true;
     if (number % divider === 0) return false;
     return iter(number, divider + 1);
